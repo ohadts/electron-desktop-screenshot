26bfe628990503b4485f2ee851d16bc489474d7b
ohad tsivoni <ohadts@gmail.com>
2022-06-20 02:34:25 +0300

    finish

--- index.js
+++ index.js
@@ -1,47 +1,86 @@
 const electron = require('electron');
-const {app, BrowserWindow, Tray, Menu, ipcMain, desktopCapturer, screen} = electron;
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu,
+  ipcMain,
+  desktopCapturer,
+  screen,
+  dialog,
+  nativeImage,
+  clipboard,
+} = electron;
+
+const { writeFile } = require("fs");
 
 let tray = null;
 
-const createCaptureWindow = ()=>{
-    const captureWindow = new BrowserWindow({   
-        width: 1650,  
-        height: 1050,  
-        webPreferences:{
-            preload: `${__dirname}/preload.js`
-        }
-    });
-    captureWindow.loadURL(`file://${__dirname}/mainWindow.html`); 
-    return captureWindow;
-}
+const createCaptureWindow = () => {
+  const captureWindow = new BrowserWindow({
+    width: 1650,
+    height: 1050,
+    webPreferences: {
+      preload: `${__dirname}/preload.js`,
+    },
+  });
+  captureWindow.loadURL(`file://${__dirname}/mainWindow.html`);
+  return captureWindow;
+};
 
-app.whenReady().then(()=>{
-    tray = new Tray(`${__dirname}/iconTemplate.png`);
-    desktopCapturer.getSources({ 
-        types: ['screen'], thumbnailSize: {    
-            height: screen.getAllDisplays()[1].size.height,  
-            width: screen.getAllDisplays()[1].size.width
-        } 
-    }).then(sources => { 
-   
-        const contextMenu = Menu.buildFromTemplate(
-            sources.map((source)=>{ return {label: source.name, click:()=>{
-                const captureWindow = createCaptureWindow();
-                captureWindow.webContents.once('dom-ready', () => {
-                    captureWindow.webContents.send("screenshot", source.thumbnail.toDataURL());
-                });
-                
-            }}})
-            )
-            tray.setToolTip('This is my application.')
-            tray.setContextMenu(contextMenu) 
-        
-        })
-
+app.whenReady().then(() => {
+  tray = new Tray(`${__dirname}/iconTemplate.png`);
+  desktopCapturer
+    .getSources({
+      types: ["screen"],
+      thumbnailSize: {
+        height: screen.getAllDisplays()[1].size.height,
+        width: screen.getAllDisplays()[1].size.width,
+      },
     })
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    .then((sources) => {
+      const contextMenu = Menu.buildFromTemplate(
+        sources.map((source) => {
+          return {
+            label: source.name,
+            click: () => {
+              const captureWindow = createCaptureWindow();
+              captureWindow.webContents.once("dom-ready", () => {
+                captureWindow.webContents.send(
+                  "screenshot",
+                  source.thumbnail.toDataURL()
+                );
+              });
+            },
+          };
+        }).concat([
+            {type: 'separator'}, {role: 'quit'}
+        ])
+      );
+      tray.setToolTip("This is my application.");
+      tray.setContextMenu(contextMenu);
+    });
+});
+app.on("window-all-closed", () => {});
+
+ipcMain.handle("SAVE_FILE", async (event, base64) => {
+  const { canceled, filePath } = await dialog.showSaveDialog({
+    buttonLabel: "Save image",
+    defaultPath: `screenshot-${Date.now()}.png`,
+  });
+  if (!canceled && filePath) {
+    writeFile(filePath, base64, "base64", () => {
+      console.log("image saved successfully!");
+      event.sender.destroy();
+    });
+  }
+});
+
+ipcMain.handle("COPY_IMAGE", async (event, base64) => {
+  const image = nativeImage.createFromDataURL(base64);
+  clipboard.writeImage(image);
+  event.sender.destroy();
+});
 
 
 
--- preload.js
+++ preload.js
@@ -3,12 +3,12 @@ const {
     contextBridge,
     ipcRenderer
 } = require("electron");
+const { writeFile } = require("fs");
 
-contextBridge.exposeInMainWorld(
-    "electron", {
-        send: ipcRenderer.send,
-        onAsynchronousMessage: (fn) => {
-            ipcRenderer.on('screenshot', (event, ...args) => fn(...args));
-          }
-    }
-);
+contextBridge.exposeInMainWorld("electron", {
+  onAsynchronousMessage: (fn) => {
+    ipcRenderer.on("screenshot", (event, ...args) => fn(...args));
+  },
+  saveFile: (base64) => ipcRenderer.invoke("SAVE_FILE", base64),
+  copyImage: (base64) => ipcRenderer.invoke("COPY_IMAGE", base64),
+});
